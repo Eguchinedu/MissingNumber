a757c2f832970b1fed4bc89881ff8d654d690e32
Egu Chinedu <eguchinedu18@gmail.com>
2023-11-15 22:30:18 +0100

    Add project files.

---
+++ MissingNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class MissingNumberSolution
+{
+    public static int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+
+        int expectedSum = (n * (n + 1)) / 2;
+        int actualSum = 0;
+        foreach (int num in nums)
+        {
+            actualSum += num;
+        }
+        var missingNum = expectedSum - actualSum;
+
+        return missingNum;
+    }
+
+    // The main method where the program starts execution
+    static void Main()
+    {
+        
+        int[] nums1 = { 3, 0, 1 };
+        Console.WriteLine("Missing Num is: " + MissingNumber(nums1));
+
+       
+        int[] nums2 = { 0, 1 };
+        Console.WriteLine("Missing Num is: " + MissingNumber(nums2));
+
+        // Example 3
+        int[] nums3 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+        Console.WriteLine("Missing Num is: " + MissingNumber(nums3));
+    }
+}
