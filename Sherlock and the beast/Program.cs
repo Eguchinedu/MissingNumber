990b17a571bb0fa879723dbedefa636d2782e1ca
Egu Chinedu <eguchinedu18@gmail.com>
2023-11-16 14:20:04 +0100

    made minor changes

--- Sherlock and the beast/Program.cs
+++ Sherlock and the beast/Program.cs
@@ -5,23 +5,23 @@ class Solution
     public static void decentNumber(int n)
     {
         // Here i am looking for the maximum number of 5's needed
-        int maxOfFives = n;
+        int lengthOfFives = n;
 
-        // Then i decrease maxOfFives  until it is divisible by 3
-        while (maxOfFives % 3 != 0)
+        // Then i decrease lengthOfFives by 5 until it is divisible by 3
+        while (lengthOfFives % 3 != 0)
         {
-            maxOfFives -= 5;
+            lengthOfFives -= 5;
         }
 
-        // Now here if the number of 5's is negative or zero, then a decent number is not possible
-        if (maxOfFives < 0)
+        // Now here if the lengthOFives of 5's is negative or zero, then a decent number is not possible
+        if (lengthOfFives < 0)
         {
             Console.WriteLine("-1");
         }
         else
         {
             //or else i print the decent number with maxFives 5's and (n - maxFives) 3's
-            Console.WriteLine(new string('5', maxOfFives) + new string('3', n - maxOfFives));
+            Console.WriteLine(new string('5', lengthOfFives) + new string('3', n - lengthOfFives));
         }
     }
 
